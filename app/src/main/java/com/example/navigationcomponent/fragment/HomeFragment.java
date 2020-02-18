441d8af9d5a2bfdb83e0191de4c0eebf59f87085
yourajeet17 <yourajeet17@gmail.com>
2020-02-18 09:46:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navigationcomponent/fragment/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.example.navigationcomponent.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.navigationcomponent.R;
+
+public class HomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.frag_home,container,false);
+        return view;
+    }
+}
